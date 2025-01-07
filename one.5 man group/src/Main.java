edf99ad35ca5ba132b8866db30a065a42f7e9d46
jingj <348820028@gapps.yrdsb.ca>
2025-01-07 09:57:55 -0500

    day 1

--- one.5 man group/src/Main.java
+++ one.5 man group/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < 10000; i++) {
             System.out.println("Jacob got a 9%");
+            System.out.println("d)");
         }
     }
     }
