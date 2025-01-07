8ec9e487a5dfa3b3d5da715ed569af699a9adc7b
jingj <348820028@gapps.yrdsb.ca>
2025-01-07 16:03:29 -0500

    day 1

--- one.5 man group/src/Main.java
+++ one.5 man group/src/Main.java
@@ -1,10 +1,36 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 10000; i++) {
-            System.out.println("Jacob got a 9%");
-            System.out.println("d)");
+       System.out.println("enter 1 to play vs a person or enter 2 to vs our bot");
+       Scanner sc = new Scanner(System.in);
+       int choice = sc.nextInt();
+       while (choice != 1 && choice != 2) {
+           System.out.println("invalid input");
+           System.out.println("enter 1 to play vs a person or enter 2 to vs our bot");
+           choice = sc.nextInt();
+       }
+       int max = 10;
+       int min = 1;
+       switch (choice) {
+           case 1:
+               System.out.println("enter player 1's name");
+               String player1 = sc.next();
+               System.out.println("enter player 2's name");
+               String player2 = sc.next();
+               for (int i = 0; i < 7; i++){
+                   int random = (int)(Math.random()*(max-min+1)+min);
+                   if ()
+                   System.out.println("red");
+               }
+               break;
+           case 2:
+               System.out.println("enter player 1's name");
+               String player = sc.next();
+
+
+               break;
+
+       }
         }
     }
-    }
+
