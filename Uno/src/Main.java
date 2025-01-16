6cf87a3bd38676e540269bfb94674b1bdbc30c0d
dotty728 <348820028@gapps.yrdsb.ca>
2025-01-16 05:34:29 +0000

    Added the randomized deck, made it so the top card can't be a special card, gave the cards to P1 and P2, polished ashowed the hands of the players

--- Uno/src/Main.java
+++ Uno/src/Main.java
@@ -1,36 +1,80 @@
-
+        
+        import java.util.ArrayList;
         import java.util.Scanner;
+        import java.util.Collections;
+        
 public class Main {
     public static void main(String[] args) {
-       System.out.println("enter 1 to play vs a person or enter 2 to vs our bot");
-       Scanner sc = new Scanner(System.in);
-       int choice = sc.nextInt();
-       while (choice != 1 && choice != 2) {
-           System.out.println("invalid input");
+        Scanner sc = new Scanner (System.in);
+        ArrayList<String> cards = new ArrayList<>();
+         String[] colors = {"B", "R", "G", "Y"};
+         String[] wilds = {"Wild", "Draw 4"};
+         String[] specials = {"Draw two", "skip"};
+         for (String color : colors){
+            cards.add(color + " " + 0);
+            for (String special : specials) {
+                cards.add(color + " " + special);
+                cards.add(color + " " + special);
+            }
+        
+    
+            for (int i = 1; i<=9; i++){
+                cards.add(color + " " + i);
+                cards.add(color + " " + i);
+            }
+            for (String wild : wilds){
+            for (int j = 0; j<4; j++){
+                cards.add(wild);
+            }
+            }
+            
+
+            
+        Collections.shuffle(cards);
+        String topcard = cards.get(0);
+        while (topcard.contains("Wild")||topcard.contains("Draw 4")||topcard.contains("skip")||topcard.contains("Draw two")){
+            Collections.shuffle(cards);
+            topcard = cards.get(0);
+        }
+        
+        
            System.out.println("enter 1 to play vs a person or enter 2 to vs our bot");
-           choice = sc.nextInt();
-       }
-       int max = 10;
-       int min = 1;
+          int choice = sc.nextInt();
        switch (choice) {
            case 1:
+           ArrayList<String> P1 = new ArrayList<>();
+           ArrayList<String> P2 = new ArrayList<>();
                System.out.println("enter player 1's name");
-               String player1 = sc.next();
+               String player1 = sc.nextLine();
+               sc.nextLine();
                System.out.println("enter player 2's name");
                String player2 = sc.next();
-               for (int i = 0; i < 7; i++){
-                   int random = (int)(Math.random()*(max-min+1)+min);
-                   if ()
-                   System.out.println("red");
-               }
+              for (int i = 1; i <=7; i++){
+                P1.add(cards.remove(0));
+                P2.add(cards.remove(0));
+              }
+              System.out.println("Players 1's hand is" + P1);
+              System.out.println("enter 1 to show player 2's hand");
+              int continues = sc.nextInt();
+              while (continues != 1){
+                System.out.println("enter 1 to show player 2's hand");
+                continues = sc.nextInt();
+              } 
+              System.out.println("Players 2's hand is" + P2);
+              System.out.println("the first card is " + topcard);
                break;
-           case 2:
-               System.out.println("enter player 1's name");
-               String player = sc.next();
 
-
-               break;
-
-       }
+               case 2;
+        
+                    }
+                  
+                }
+           
+            }
         }
-}
+            
+        
+    
+
+    
+    
